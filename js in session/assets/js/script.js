14321c9624bb6a8ed6cf023a10e87db79fc67e9f
Yousef elsayed <yousefelsayed0077@gmail.com>
2025-07-22 20:26:55 +0300

    Add files via upload

---
+++ js in session/assets/js/script.js
@@ -0,0 +1,14 @@
+var listItems = document.querySelectorAll("#myList li");
+
+listItems.forEach((li) => {
+    li.addEventListener("click", () => {
+
+        listItems.forEach((item) => {
+            item.style.color = "";
+            item.style.fontWeight = "";
+        });
+
+        li.style.color = "red";
+        li.style.fontWeight = "bold";
+    });
+});
